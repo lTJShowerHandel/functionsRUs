a77503b55505dc31b9bfac122292741e83cfc1cd
lTJShowerHandel <blanesantilli@gmail.com>
2025-02-26 17:35:43 -0700

    Create function3.py
    commented description of the 2nd function

---
+++ function3.py
@@ -0,0 +1,11 @@
+# Display menu and return choice. 
+# Store in variable and use this value to determine which function to call next.
+
+
+
+# options
+# start the program
+# quit the program
+#   choose a team (hardcoded teams)
+#   input your teams (this option allows the user to input team names)
+#       how many games are played this season
