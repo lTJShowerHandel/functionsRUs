1f02386fbfd63c3b585d849398d8ef4edd42e833
henrytuttle16 <166653129+henrytuttle16@users.noreply.github.com>
2025-02-26 18:58:34 -0700

    Added function 3 loop

--- mainProg.py
+++ mainProg.py
@@ -1,20 +1,41 @@
-
-
+# Import functions
+from function1 import welcomeMessage
+from function3 import function3
 
 welcomeMessage()
 
 lstTeams = menuOptions()
 #returns lstTeams
 
-
-homeTeam = function3(lstTeams)
-
+# Function 3. Run first for home team, then loop for away teams
 
 
+sHomeTeam = function3(lstTeams)
+
+# Removes selected team from list
+if sHomeTeam in lstTeams:
+    lstTeams.remove( sHomeTeam)
+else:
+    print( "Team not found, try again.")
+
+# Intialize list of games
 lstGames= []
 
-lstGames.append(function4(homeTeam,awayteam)) #adds generated scores to lst of games
+# Loop and call function 3 for away teams
+while len(lstTeams) > 0 :  
+    sAwayTeam = function3(lstTeams)
 
+    # Remove selected team
+    if sAwayTeam in lstTeams:
+        lstTeams.remove(sAwayTeam)
+    else:
+        print("Team not found, try again.")
 
-display_team_record(lstGames,homeTeam)
+    lstGames.append(function4(sAwayTeam))
+    
+
+# Function 4
+
+#Function 5
+display_team_record(lstGames, sHomeTeam)
 
