a46fa990b9dcd80a624295b189bbdfc2c363d3f6
aarongarry <apgarry99@gmail.com>
2025-02-26 18:39:05 -0700

    Main prog

---
+++ mainProg.py
@@ -0,0 +1,20 @@
+
+
+
+welcomeMessage()
+
+lstTeams = menuOptions()
+#returns lstTeams
+
+
+homeTeam = function3(lstTeams)
+
+
+
+lstGames= []
+
+lstGames.append(function4(homeTeam,awayteam)) #adds generated scores to lst of games
+
+
+display_team_record(lstGames,homeTeam)
+
